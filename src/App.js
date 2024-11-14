8b4ba8650bd357e4375dece6ba7d8d82d597724e
talicumivm <talita.vasquez@alumnos.ucentral.cl>
2024-11-14 12:49:52 -0300

    Creacion de Carrusel para imagenes del inicio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import ContactForm from "./components/ContactForm"; // Importa el componente ContactForm
+import ContactForm from "./components/ContactForm";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css'; 
-import producto1 from './assets/imagenes/producto1.jpg';
-import producto2 from './assets/imagenes/producto5.jpg';
-import producto3 from './assets/imagenes/producto3.jpg';
-import producto4 from './assets/imagenes/producto4.jpg';
+import ProductCarousel from './components/ProductCarousel';
+
+// Importa las imágenes para cada producto
+import producto1_1 from './assets/imagenes/producto1.jpg';
+import producto1_2 from './assets/imagenes/producto1.1.jpg';
+import producto1_3 from './assets/imagenes/producto1.2.jpg';  
+
+import producto2_1 from './assets/imagenes/producto5.jpg';
+import producto2_2 from './assets/imagenes/producto5.1.jpg';
+import producto2_3 from './assets/imagenes/producto5.2.jpg';  
+
+import producto3_1 from './assets/imagenes/producto3.jpg';
+import producto3_2 from './assets/imagenes/producto3.1.jpg';
+import producto3_3 from './assets/imagenes/producto3.2.jpg';
+
+
+import producto4_1 from './assets/imagenes/producto4.jpg';
+import producto4_2 from './assets/imagenes/producto4.1.jpg';
+import producto4_3 from './assets/imagenes/producto4.2.jpg';
+
+
 
 function App() {
   return (
@@ -17,7 +34,7 @@ function App() {
       <Routes>
         <Route path="/" element={
           <>
-            <h4>Envío a traves de Starken o Bluexpress</h4>
+            <h4>Envío a través de Starken o Bluexpress</h4>
 
             {/* Título y sección de consulta */}
             <div className="consulta">
@@ -27,25 +44,25 @@ function App() {
             {/* Sección de productos */}
             <div className="productos">
               <div className="producto">
-                <img src={producto1} alt="Producto 1" className="producto-imagen" />
+                <ProductCarousel images={[producto1_1, producto1_2, producto1_3]} />
                 <h3>Palmera Chamadoera</h3>
                 <p>$6500</p>
               </div>
 
               <div className="producto">
-                <img src={producto2} alt="Producto 5" className="producto-imagen" />
+                <ProductCarousel images={[producto2_1, producto2_2, producto2_3]} />
                 <h3>Singonio Plateado</h3>
                 <p>$6000</p>
               </div>
-ñ
+
               <div className="producto">
-                <img src={producto3} alt="Producto 3" className="producto-imagen" />
+                <ProductCarousel images={[producto3_1, producto3_2, producto3_3]} />
                 <h3>Planta de Incienso</h3>
                 <p>$3000</p>
               </div>
 
               <div className="producto">
-                <img src={producto4} alt="Producto 4" className="producto-imagen" />
+                <ProductCarousel images={[producto4_1, producto4_2,producto4_3]} />
                 <h3>Violeta de Persia</h3>
                 <p>$6000</p>
               </div>
---
+++ src/components/ProductCarousel.js
@@ -0,0 +1,24 @@
+// ProductCarousel.js
+import React, { useState } from 'react';
+
+function ProductCarousel({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div className="carousel">
+      <button className="carousel-button prev" onClick={prevSlide}>❮</button>
+      <img src={images[currentIndex]} alt={`Producto ${currentIndex + 1}`} className="carousel-image" />
+      <button className="carousel-button next" onClick={nextSlide}>❯</button>
+    </div>
+  );
+}
+
+export default ProductCarousel;
