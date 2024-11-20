8413adcb6d15db0095cb5caaeda4320530d46a6e
dlwlrmjlo <joakomask@gmail.com>
2024-11-20 11:41:29 -0300

    whatsapp wea

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -12,10 +12,17 @@ function ProductCarousel({ images }) {
     setCurrentIndex((currentIndex - 1 + images.length) % images.length);
   };
 
+  const handleImageClick = () => {
+    const phone = '56973016700'; // Reemplaza con el número de WhatsApp, sin el '+'
+    const message = 'sexo';
+    const whatsappLink = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappLink, '_blank');
+  };
+
   return (
     <div className="carousel">
       <button className="carousel-button prev" onClick={prevSlide}>❮</button>
-      <img src={images[currentIndex]} alt={`Producto ${currentIndex + 1}`} className="carousel-image" />
+      <img src={images[currentIndex]} alt={`Producto ${currentIndex + 1}`} onClick={handleImageClick} style={{curso:'pointer'}} className="carousel-image" />
       <button className="carousel-button next" onClick={nextSlide}>❯</button>
     </div>
   );
